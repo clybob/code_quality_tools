125e67c5d987eabdbaf9867fe4fab5efe1593d99
Romulo Costa Tavares <romulo.tavares@corp.globo.com>
2012-10-02 19:27:54 -0300

    Created method to get all errors
    - Created class CodeQualityCheck to group methods

--- code_quality_tools/code_quality_tools.py
+++ code_quality_tools/code_quality_tools.py
@@ -2,87 +2,90 @@ import subprocess
 import re
 
 
-def execute(tool, path, output_file, options):
-    command = [tool, path]
-    command.extend(options)
-    output = execute_shell_command(command, output_file)
-    return output
+class CodeQualityCheck():
 
+    def __execute(self, tool, path, output_file, options):
+        command = [tool, path]
+        command.extend(options)
+        output = self.__execute_shell_command(command, output_file)
+        return output
 
-def execute_shell_command(command, stdout=None):
-    if stdout is None:
-        lines = get_errors(command, stdout)
-    else:
-        lines = save_output_file_and_get_errors(command, stdout)
+    def __execute_shell_command(self, command, stdout=None):
+        if stdout is None:
+            lines = self.__get_errors(command, stdout)
+        else:
+            lines = self.__save_output_file_and_get_errors(command, stdout)
 
-    count_errors = len(lines)
-    errors = {'total_errors': count_errors, 'list_errors': lines}
+        count_errors = len(lines)
+        errors = {'total_errors': count_errors, 'list_errors': lines}
 
-    return errors
+        return errors
 
+    def __get_errors(self, command, stdout):
+        stdout = subprocess.PIPE
+        result = subprocess.Popen(command, stdout=stdout)
+        output, error = result.communicate()
+        output_lines = output.splitlines()
 
-def get_errors(command, stdout):
-    stdout = subprocess.PIPE
-    result = subprocess.Popen(command, stdout=stdout)
-    output, error = result.communicate()
+        lines = [line for line in output_lines if self.__validate_line(line)]
+        return lines
 
-    # Discard empty lines
-    lines = [line for line in output.splitlines() if validate_line(line)]
-    return lines
+    def __validate_line(self, line):
+        return line and ' errors' not in line
 
+    def __save_output_file_and_get_errors(self, command, stdout):
+        output_file = open(stdout, 'w')
+        result = subprocess.Popen(command, stdout=output_file)
+        output, error = result.communicate()
+        output_file.close()
 
-def validate_line(line):
-    return line and ' errors' not in line
+        output_file = open(stdout, 'r')
+        lines = output_file.read().splitlines()
+        output_file.close()
+        return lines
 
+    def get_pep8_errors(self, path='.', output_file=None, options=[]):
+        return self.__execute('pep8', path, output_file, options)
 
-def save_output_file_and_get_errors(command, stdout):
-    output_file = open(stdout, 'w')
-    result = subprocess.Popen(command, stdout=output_file)
-    output, error = result.communicate()
-    output_file.close()
+    def get_pyflakes_errors(self, path='.', output_file=None, options=[]):
+        return self.__execute('pyflakes', path, output_file, options)
 
-    output_file = open(stdout, 'r')
-    lines = output_file.read().splitlines()
-    output_file.close()
-    return lines
+    def get_jshint_errors(self, path='.', output_file=None, options=[]):
+        return self.__execute('jshint', path, output_file, options)
 
+    def get_csslint_errors(self, path='.', output_file=None, options=[]):
+        options.extend(['--format=compact'])
+        return self.__execute('csslint', path, output_file, options)
 
-def get_pep8_errors(path='.', output_file=None, options=[]):
-    return execute('pep8', path, output_file, options)
+    def get_clonedigger_errors(self, path='.', output_file='output_clonedigger.html', options=[]):
+        options.extend(['--output=%s' % output_file])
+        self.__execute('clonedigger', path, None, options)
 
+        output_clonedigger = open(output_file)
+        result_clonedigger = output_clonedigger.read()
+        output_clonedigger.close()
 
-def get_pyflakes_errors(path='.', output_file=None, options=[]):
-    return execute('pyflakes', path, output_file, options)
+        regex_percentage = '(?<=duplicates \()\d+'
+        regex_total = '(?<=Clones detected: )\d+'
 
+        porcentage = re.search(regex_percentage, result_clonedigger)
+        porcentage = int(porcentage.group())
 
-def get_jshint_errors(path='.', output_file=None, options=[]):
-    return execute('jshint', path, output_file, options)
+        total_clones = re.search(regex_total, result_clonedigger)
+        total_clones = int(total_clones.group())
 
+        result = {
+            'total_clones': total_clones,
+            'percentage_clones': porcentage
+        }
+        return result
 
-def get_jslint_errors(path='.', output_file=None, options=[]):
-    options.extend(['--browser=true'])
-    return execute('jslint', path, output_file, options)
-
-
-def get_csslint_errors(path='.', output_file=None, options=[]):
-    options.extend(['--format=compact'])
-    return execute('csslint', path, output_file, options)
-
-
-def get_clonedigger_errors(path='.', output_file='output_clonedigger.html',
-    options=[]):
-    options.extend(['--output=%s' % output_file])
-    execute('clonedigger', path, None, options)
-
-    output_clonedigger = open(output_file)
-    result_clonedigger = output_clonedigger.read()
-    output_clonedigger.close()
-
-    porcentage = re.search('(?<=duplicates \()\d+', result_clonedigger)
-    porcentage = int(porcentage.group())
-
-    total_clones = re.search('(?<=Clones detected: )\d+', result_clonedigger)
-    total_clones = int(total_clones.group())
-
-    result = {'total_clones': total_clones, 'percentage_clones': porcentage}
-    return result
+    def get_all_errors(self, path='.'):
+        errors = {
+            'pep8': self.get_pep8_errors(path),
+            'pyflakes': self.get_pyflakes_errors(path),
+            'jshint': self.get_jshint_errors(path),
+            'csslint': self.get_csslint_errors(path),
+            'clonedigger': self.get_clonedigger_errors(path)
+        }
+        return errors
--- code_quality_tools/tests/test_code_quality_tools.py
+++ code_quality_tools/tests/test_code_quality_tools.py
@@ -4,49 +4,50 @@ import unittest
 import sys
 from os.path import join, abspath, dirname
 sys.path.insert(0, abspath(join(dirname(''))))
-import code_quality_tools
+from code_quality_tools import CodeQualityCheck
 
 
 class TestCodeQualityTools(unittest.TestCase):
     path_fixtures = 'tests/fixtures/'
+    check = CodeQualityCheck()
 
     def test_get_pep8_errors(self):
         path_fixture = self.path_fixtures + 'pep8.py'
-        errors = code_quality_tools.get_pep8_errors(path_fixture)
+        errors = self.check.get_pep8_errors(path_fixture)
         self.assertEqual(errors['total_errors'], 3)
         self.assertEqual(len(errors['list_errors']), 3)
 
     def test_get_pyflakes_errors(self):
         path_fixture = self.path_fixtures + 'pyflakes.py'
-        errors = code_quality_tools.get_pyflakes_errors(path_fixture)
+        errors = self.check.get_pyflakes_errors(path_fixture)
         self.assertEqual(errors['total_errors'], 2)
         self.assertEqual(len(errors['list_errors']), 2)
 
     def test_get_jshint_errors(self):
         path_fixture = self.path_fixtures + 'jshint.js'
-        errors = code_quality_tools.get_jshint_errors(path_fixture)
+        errors = self.check.get_jshint_errors(path_fixture)
         self.assertEqual(errors['total_errors'], 2)
         self.assertEqual(len(errors['list_errors']), 2)
 
-    def test_get_jslint_errors(self):
-        path_fixture = self.path_fixtures + 'jslint.js'
-        errors = code_quality_tools.get_jslint_errors(path_fixture)
-        import pdb; pdb.set_trace()
-        self.assertEqual(errors['total_errors'], 5)
-        self.assertEqual(len(errors['list_errors']), 5)
-
     def test_get_csslint_errors(self):
         path_fixture = self.path_fixtures + 'csslint.css'
-        errors = code_quality_tools.get_csslint_errors(path_fixture)
+        errors = self.check.get_csslint_errors(path_fixture)
         self.assertEqual(errors['total_errors'], 6)
         self.assertEqual(len(errors['list_errors']), 6)
 
     def test_get_clonedigger_errors(self):
         path_fixture = self.path_fixtures + 'clonedigger.py'
-        errors = code_quality_tools.get_clonedigger_errors(path_fixture)
+        errors = self.check.get_clonedigger_errors(path_fixture)
         self.assertEqual(errors['total_clones'], 1)
         self.assertEqual(errors['percentage_clones'], 100)
 
+    def test_get_all_errors(self):
+        errors = self.check.get_all_errors(self.path_fixtures)
+        self.assertEqual(len(errors['pep8']), 2)
+        self.assertEqual(len(errors['pyflakes']), 2)
+        self.assertEqual(len(errors['jshint']), 2)
+        self.assertEqual(len(errors['csslint']), 2)
+        self.assertEqual(len(errors['clonedigger']), 2)
 
 if __name__ == '__main__':
     unittest.main()
