2300172a46ae8540dfd7c1644e228e79552ebb18
Romulo Costa Tavares <romulo.tavares@corp.globo.com>
2012-10-03 20:35:42 -0300

    Created setup.py

---
+++ code_quality_tools/setup.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup(
+    name='code_quality_tools',
+    version='0.1.0',
+    author='Romulo Tavares',
+    author_email='clybob@hotmail.com',
+    packages=['code_quality_tools'],
+    license='GPL',
+    url='https://github.com/clybob/code_quality_tools.git',
+    description='code_quality_tools is a little API, in python, ' +
+                'to collect some data about code quality of your ' +
+                'source code (python, css, js).',
+    long_description=open('README.md').read(),
+    install_requires=['pep8', 'pyflakes', 'clonedigger']
+)
